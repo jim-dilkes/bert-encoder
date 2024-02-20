be400d5b825daaddcf95a684bdff9e92ea41969a
jim <jsdilkes@gmail.com>
2024-02-20 17:09:17 +0000

    Refactoring, added time tracking

--- src/data_ops.py
+++ src/data_ops.py
@@ -4,8 +4,40 @@ import torch
 from einops import rearrange
 
 
-def mask_tokens(input_batch: torch.Tensor, mask_id:int, pad_id:int, mask_prob:float=0.1,
-                vocab_low_high:tuple[int,int]=(5,15000)) -> tuple[torch.Tensor, torch.Tensor]:
+def seconds_to_ms(seconds: float) -> str:
+    """Convert seconds to a string in the format 'mm:ss'."""
+    minutes = int(seconds // 60)
+    seconds = int(seconds % 60)
+    return f"{minutes:02d}:{seconds:02d}"
+
+
+def create_directory(directory: str, reset: bool = False):
+    """Delete and recreate a directory"""
+    if os.path.exists(directory):
+        if reset:
+            shutil.rmtree(directory)  # Delete the directory and its contents
+            os.makedirs(directory)
+    else:
+        os.makedirs(directory)
+
+
+def gather_files(directory: str, file_extension: str = None) -> list[str]:
+    """Gather a list of filepaths from a directory and its subdirectories. Optionally filter by file_extension."""
+    file_list = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file_extension is None or file[-len(file_extension) :] == file_extension:
+                file_list.append(os.path.join(root, file))
+    return file_list
+
+
+def mask_tokens(
+    input_batch: torch.Tensor,
+    mask_id: int,
+    pad_id: int,
+    mask_prob: float = 0.1,
+    vocab_low_high: tuple[int, int] = (5, 15000),
+) -> tuple[torch.Tensor, torch.Tensor]:
     """Mask random tokens in a batch of input data."""
 
     assert mask_prob <= 1.0 and mask_prob >= 0
@@ -14,42 +46,38 @@ def mask_tokens(input_batch: torch.Tensor, mask_id:int, pad_id:int, mask_prob:fl
     ## Select tokens for masking
     mask_probabilities = torch.full(input_batch.shape, mask_prob, device=use_device)
     padding_mask = input_batch.eq(pad_id)
-    mask_probabilities.masked_fill_(padding_mask,0)
+    mask_probabilities.masked_fill_(padding_mask, 0)
     masked_tokens_bool = torch.bernoulli(mask_probabilities).bool()
-    
-    
+
     ## Replace selected tokens
     masked_input_batch = input_batch.clone()
 
     # 0.8 replace masked input tokens with mask_id
-    replaced_tokens_bool = torch.bernoulli(torch.full(input_batch.shape, 0.8, device=use_device)).bool() & masked_tokens_bool
+    replaced_tokens_bool = (
+        torch.bernoulli(torch.full(input_batch.shape, 0.8, device=use_device)).bool()
+        & masked_tokens_bool
+    )
     masked_input_batch.masked_fill_(replaced_tokens_bool, mask_id)
 
     # 0.1 replace masked input tokens with random word
-    random_tokens_bool = torch.bernoulli(torch.full(input_batch.shape, 0.5, device=use_device)).bool() & masked_tokens_bool & ~replaced_tokens_bool
-    random_words = torch.randint(vocab_low_high[0],vocab_low_high[1], input_batch.shape, dtype=torch.long, device=use_device)
+    random_tokens_bool = (
+        torch.bernoulli(torch.full(input_batch.shape, 0.5, device=use_device)).bool()
+        & masked_tokens_bool
+        & ~replaced_tokens_bool
+    )
+    random_words = torch.randint(
+        vocab_low_high[0],
+        vocab_low_high[1],
+        input_batch.shape,
+        dtype=torch.long,
+        device=use_device,
+    )
     masked_input_batch[random_tokens_bool] = random_words[random_tokens_bool]
 
     return masked_input_batch, masked_tokens_bool
 
 
-def reset_directory(directory:str):
-    """Delete and recreate a directory"""
-    if os.path.exists(directory):
-        shutil.rmtree(directory)  # Delete the directory and its contents
-    os.makedirs(directory)
-
-def gather_files(directory:str, file_extension:str=None) -> list[str]:
-    """Gather a list of filepaths from a directory and its subdirectories. Optionally filter by file_extension."""
-    file_list = []
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            if file_extension is None or file[-len(file_extension):] == file_extension:
-                file_list.append(os.path.join(root, file)) 
-    return file_list
-
-
-class DataLoader():
+class DataLoader:
     """
     DataLoader for loading and iterating through batches of data from a list of filepaths.
     - Loads data from filepaths in order, splitting each file into batches of size batch_size
@@ -58,8 +86,16 @@ class DataLoader():
     - The hanging batch is prepended to the next file's data
     - If there is not enough data to fill a final batch, the final hanging batch is ignored to avoid a partial batch
     """
-    def __init__(self, filepaths:list[str], batch_size:int, file_idx:int | None = None, shuffle_contents:bool=True, device:str='cpu'):
-        """ Args:
+
+    def __init__(
+        self,
+        filepaths: list[str],
+        batch_size: int,
+        file_idx: int | None = None,
+        shuffle_contents: bool = True,
+        device: str = "cpu",
+    ):
+        """Args:
         - filepaths: list of filepaths to load data from
         - batch_size: number of examples per batch
         - file_idx: index of the first file to load
@@ -81,7 +117,7 @@ class DataLoader():
     def __iter__(self):
         # If DataLoader is initially set to a file_idx, start from that file
         if self.file_idx is not None and self.batch_counter == 0:
-            self.file_idx = self.file_idx 
+            self.file_idx = self.file_idx
         # If DataLoader is reset part way through an epoch, start from 0
         else:
             self.file_idx = 0
@@ -93,27 +129,27 @@ class DataLoader():
         self._load_next_buffer()
 
         return self
-    
+
     def __next__(self):
         if self.buffer_idx >= self.buffer_size:
             self.file_idx += 1
             self._load_next_buffer()
-            
+
         if self.example_buffer is None:
             if self.hanging_batch is not None:
                 final_batch = self.hanging_batch
                 self.hanging_batch = None
                 return final_batch
-            self.file_idx = None # Reset file_idx to None
+            self.file_idx = None  # Reset file_idx to None
             raise StopIteration
-        
+
         batch = self.example_buffer[self.buffer_idx]
         self.buffer_idx += 1
         self.batch_counter += 1
         self.example_counter += self.batch_size
         return self.batch_counter, batch, self.file_idx
 
-    def _load_next_buffer(self)->torch.Tensor | None:
+    def _load_next_buffer(self) -> torch.Tensor | None:
         def _get_next_file_data():
             if self.file_idx >= self.n_files:
                 return None
@@ -125,25 +161,36 @@ class DataLoader():
                 if self.shuffle_contents:
                     data[torch.randperm(data.size(0))]
                 return data
-        
-        print(f'Loading file {self.file_idx}/{self.n_files} | Done {self.example_counter} examples in {self.batch_counter} batches')
+
+        print(
+            f"Loading file {self.file_idx}/{self.n_files} | Done {self.example_counter} examples in {self.batch_counter} batches"
+        )
 
         data = _get_next_file_data()
-        self.example_buffer, self.hanging_batch = self._prepare_batches(data, self.batch_size, self.hanging_batch)
-        self.buffer_size = len(self.example_buffer) if self.example_buffer is not None else 0
+        self.example_buffer, self.hanging_batch = self._prepare_batches(
+            data, self.batch_size, self.hanging_batch
+        )
+        self.buffer_size = (
+            len(self.example_buffer) if self.example_buffer is not None else 0
+        )
         self.buffer_idx = 0
-        
+
     @staticmethod
-    def _prepare_batches(data, batch_size:int, hanging_batch:torch.Tensor=None) -> tuple[torch.Tensor, torch.Tensor] | tuple [None, None]:
+    def _prepare_batches(
+        data, batch_size: int, hanging_batch: torch.Tensor = None
+    ) -> tuple[torch.Tensor, torch.Tensor] | tuple[None, None]:
         if hanging_batch is not None:
             if data is None:
-                return None, None # No more data to load, return None to force StopIteration
+                return (
+                    None,
+                    None,
+                )  # No more data to load, return None to force StopIteration
             else:
                 data = torch.cat([hanging_batch, data], dim=0)
         n_batches = len(data) // batch_size
-        hanging_batch = data[n_batches*batch_size:]
+        hanging_batch = data[n_batches * batch_size :]
         if len(hanging_batch) == 0:
             hanging_batch = None
-        data = data[:n_batches*batch_size]
-        data = rearrange(data, '(n b) s -> n b s', b=batch_size)
+        data = data[: n_batches * batch_size]
+        data = rearrange(data, "(n b) s -> n b s", b=batch_size)
         return data, hanging_batch
--- train_transformer.py
+++ train_transformer.py
@@ -1,9 +1,11 @@
-
+import math
 import os
 import time
 import torch
 import torch.optim as optim
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
 
 from src import data_ops
 from src.transformer import EncoderTransformer
@@ -12,65 +14,88 @@ import numpy as np
 
 # load tokenizer
 from tokenizers import Tokenizer
-tokenizer = Tokenizer.from_file("tokenizer_V2.json")
-padding_idx = tokenizer.encode('[PAD]').ids[0]
-mask_idx = tokenizer.encode('[MASK]').ids[0]
+
+tokenizer_filepath = ".tokenizers/tokenizer_64.json"
+tokenizer = Tokenizer.from_file(tokenizer_filepath)
+padding_idx = tokenizer.encode("[PAD]").ids[0]
+mask_idx = tokenizer.encode("[MASK]").ids[0]
 
 
-def save_checkpoint(transformer, optimizer, epoch, filepaths, file_idx,
-                    checkpoint_dir, max_checkpoints=5):
-    """ Args:
-        transformer (nn.Module): The model to save
-        optimiser (torch.optim): The optimizer to save
-        epoch (int): The current epoch
-        filepaths (list): The ordered list of filepaths
-        file_idx (int): The current file index
-        checkpoint_dir (str): The directory to save the checkpoint
-        elapsed (float): The time elapsed since training began
-        loss (float): The mean masked likelihood
-        max_checkpoints (int): The maximum number of checkpoints to keep
+def save_checkpoint(
+    transformer,
+    optimizer,
+    epoch,
+    filepaths,
+    file_idx,
+    checkpoint_dir,
+    max_checkpoints=None,
+):
+    """Args:
+    transformer (nn.Module): The model to save
+    optimiser (torch.optim): The optimizer to save
+    epoch (int): The current epoch
+    filepaths (list): The ordered list of filepaths
+    file_idx (int): The current file index
+    checkpoint_dir (str): The directory to save the checkpoint
+    elapsed (float): The time elapsed since training began
+    loss (float): The mean masked likelihood
+    max_checkpoints (int): The maximum number of checkpoints to keep
     """
-    
-    print(f'Saving checkpoint for epoch {epoch}, beginning with file {file_idx}...')
+
+    print(f"Saving checkpoint for epoch {epoch}, beginning with file {file_idx}...")
 
     # Remove old checkpoints if there are too many
-    checkpoint_filepath = os.path.join(checkpoint_dir, f'epoch{epoch}_file{file_idx}.pt')
-    checkpoint_files = data_ops.gather_files(checkpoint_dir, file_extension='.pt')
-    while len(checkpoint_files) > max_checkpoints-1:
-        checkpoint_files = sorted(checkpoint_files, key=lambda x: os.path.getctime(x))
-        oldest_file = checkpoint_files.pop(0)
-        os.remove(oldest_file)
+    checkpoint_filepath = os.path.join(
+        checkpoint_dir, f"epoch{epoch}_file{file_idx}.pt"
+    )
+    checkpoint_files = data_ops.gather_files(checkpoint_dir, file_extension=".pt")
+    if max_checkpoints is not None:
+        while len(checkpoint_files) > max_checkpoints - 1:
+            checkpoint_files = sorted(
+                checkpoint_files, key=lambda x: os.path.getctime(x)
+            )
+            oldest_file = checkpoint_files.pop(0)
+            os.remove(oldest_file)
 
     # Save the checkpoint
-    torch.save({
-        'epoch': epoch,
-        'start_file_idx': file_idx, # +1 to start from the next file
-        'ordered_filepaths': filepaths,
-        'model_state_dict': transformer.state_dict(),
-        'optimizer_state_dict': optimizer.state_dict()
-        }, checkpoint_filepath)
+    torch.save(
+        {
+            "epoch": epoch,
+            "start_file_idx": file_idx,  # +1 to start from the next file
+            "ordered_filepaths": filepaths,
+            "model_state_dict": transformer.state_dict(),
+            "optimizer_state_dict": optimizer.state_dict(),
+        },
+        checkpoint_filepath,
+    )
 
 
 def load_checkpoint(checkpoint_name, checkpoint_dir, transformer, optimizer):
-    """ Load a checkpoint and return the epoch, file index, and ordered filepaths.
-        Load model and optimizer in place, return the other values.
+    """Load a checkpoint and return the epoch, file index, and ordered filepaths.
+    Load model and optimizer in place, return the other values.
 
-        Args:
-            checkpoint_name (str): Name of the checkpoint file
-            checkpoint_dir (str): Directory where the checkpoint is stored
-            transformer (nn.Module): The model to load the checkpoint into
-            optimizer (torch.optim): The optimizer to load the checkpoint into
+    Args:
+        checkpoint_name (str): Name of the checkpoint file
+        checkpoint_dir (str): Directory where the checkpoint is stored
+        transformer (nn.Module): The model to load the checkpoint into
+        optimizer (torch.optim): The optimizer to load the checkpoint into
     """
+    if not os.path.exists(os.path.join(checkpoint_dir, checkpoint_name)):
+        raise FileNotFoundError(f"Checkpoint file {checkpoint_name} not found")
     checkpoint = torch.load(os.path.join(checkpoint_dir, checkpoint_name))
-    transformer.load_state_dict(checkpoint['model_state_dict'])
-    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-    return checkpoint['epoch'], checkpoint['start_file_idx'], checkpoint['ordered_filepaths']
+    transformer.load_state_dict(checkpoint["model_state_dict"])
+    optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+    return (
+        checkpoint["epoch"],
+        checkpoint["start_file_idx"],
+        checkpoint["ordered_filepaths"],
+    )
 
 
 def write_loss(losses, loss_epoch_dir, loss_idx):
     if not os.path.exists(loss_epoch_dir):
         os.makedirs(loss_epoch_dir)
-    with open(os.path.join(loss_epoch_dir, f'{loss_idx}.txt'), 'w') as f:
+    with open(os.path.join(loss_epoch_dir, f"{loss_idx}.txt"), "w") as f:
         # Write the losses from a list to one line per loss
         for batch_loss in losses:
             f.write(f"{batch_loss[0]},{batch_loss[1]}\n")
@@ -79,13 +104,13 @@ def write_loss(losses, loss_epoch_dir, loss_idx):
 
 ## Model Params
 vocab_size = 15000
-sequence_length = 128
+sequence_length = 64
 
 # Model Parameters
-d_embedding = 256
-d_model = 256
+d_embedding = 512
+d_model = 512
 d_ff = d_model * 4
-n_layers = 4
+n_layers = 8
 dropout = 0.1
 
 # k and v dims per head
@@ -98,80 +123,127 @@ n_heads = int(n_heads)
 
 
 ## Training Params
-root_dir = 'data_tokenized'
-batch_size = 150
+data_dir = ".data/tokenized_64"
+batch_size = 128
 n_epochs = 6
 lr = 3e-4
 
 mask_probability = 0.15
 
-checkpoint_dir = 'checkpoints'
-checkpoint_every = 20
+FLAG_LOAD_CHECKPOINT = True
+checkpoint_dir = ".checkpoints"
+checkpoint_epoch_dir = os.path.join(checkpoint_dir, "epoch")
+checkpoint_every = 10
 max_checkpoints = 5
-checkpoint_name = 'epoch0_file4.pt'
+checkpoint_name = "epoch0_file540.pt"
 
-loss_dir = 'losses'
+loss_dir = os.path.join(".metrics", "llh_loss")
 loss_idx = 0
 
-transformer = EncoderTransformer(vocab_size, sequence_length, n_layers, d_embedding, d_model, d_k, d_v, n_heads, d_ff, padding_idx, dropout).to(device)
+transformer = EncoderTransformer(
+    vocab_size,
+    sequence_length,
+    n_layers,
+    d_embedding,
+    d_model,
+    d_k,
+    d_v,
+    n_heads,
+    d_ff,
+    padding_idx,
+    dropout,
+).to(device)
 optimizer = optim.Adam(transformer.parameters())
 
-FLAG_LOAD_CHECKPOINT = False
 
 if not FLAG_LOAD_CHECKPOINT:
-    data_ops.reset_directory(checkpoint_dir)
-    data_ops.reset_directory(loss_dir)
-    
+    data_ops.create_directory(checkpoint_dir)
+    data_ops.create_directory(checkpoint_epoch_dir)
+    data_ops.create_directory(loss_dir)
+
     optimizer = optim.Adam(transformer.parameters(), lr=lr)
-    
+
     start_epoch = 0
     file_idx = 0
 
-    filepaths = data_ops.gather_files(root_dir, file_extension='.pt')
+    filepaths = data_ops.gather_files(data_dir, file_extension=".pt")
     np.random.shuffle(filepaths)
 
     checkpointed_files = 0
 else:
     optimizer = optim.Adam(transformer.parameters())
-    start_epoch, file_idx, filepaths = load_checkpoint(checkpoint_name, checkpoint_dir, transformer, optimizer)
+    start_epoch, file_idx, filepaths = load_checkpoint(
+        checkpoint_name, checkpoint_dir, transformer, optimizer
+    )
+    initial_file_idx = file_idx
     checkpointed_files = file_idx
 
 
 print("Training transformer model:")
 print(transformer)
-print(f"Number of parameters: {sum(p.numel() for p in transformer.parameters() if p.requires_grad)}\n")
+print(
+    f"Number of parameters: {sum(p.numel() for p in transformer.parameters() if p.requires_grad)}\n"
+)
 
 
 losses = []
 import time
+
 start = time.time()
+checkpoint_start = time.time()
 for epoch in range(n_epochs):
     print(f"Epoch {epoch}/{n_epochs-1}")
 
     # Load data
-    data_loader = data_ops.DataLoader(filepaths, batch_size=batch_size, file_idx=file_idx, shuffle_contents=True, device=device)
+    data_loader = data_ops.DataLoader(
+        filepaths,
+        batch_size=batch_size,
+        file_idx=file_idx,
+        shuffle_contents=True,
+        device=device,
+    )
     checkpoint_losses = []
     for i, batch, file_idx in data_loader:
         # Checkpoint
         if file_idx % checkpoint_every == 0 and file_idx > checkpointed_files:
             write_loss(checkpoint_losses, os.path.join(loss_dir, str(epoch)), loss_idx)
             loss_idx += 1
-            save_checkpoint(transformer, optimizer, epoch, filepaths, file_idx,
-                            checkpoint_dir, max_checkpoints=max_checkpoints)
-            elapsed = time.time()-start
-            print(f'Time Elapsed: {elapsed/60:.2f} | File {file_idx} ({(elapsed/(file_idx+1)):.2f}) | Mean Masked Likelihood: {loss*-1}')
+            save_checkpoint(
+                transformer,
+                optimizer,
+                epoch,
+                filepaths,
+                file_idx,
+                checkpoint_dir,
+                max_checkpoints=max_checkpoints,
+            )
+            elapsed = time.time() - start
+            checkpoint_elapsed = time.time() - checkpoint_start
+            mean_loss = np.mean([x[1] for x in checkpoint_losses])
+            print(
+                f"Total: {data_ops.seconds_to_ms(elapsed)} | Checkpoint: {data_ops.seconds_to_ms(checkpoint_elapsed)} ({elapsed/(file_idx - initial_file_idx):.2f}s/file) | Mean Masked Likelihood: {mean_loss*-1}"
+            )
+            checkpoint_start = time.time()
             checkpoint_losses = []
             checkpointed_files += checkpoint_every
         optimizer.zero_grad()
 
         # Mask tokens
-        masked_batch, masked_batch_bool = data_ops.mask_tokens(batch, mask_id=mask_idx, pad_id=padding_idx, mask_prob=mask_probability, vocab_low_high=(5,vocab_size))
+        masked_batch, masked_batch_bool = data_ops.mask_tokens(
+            batch,
+            mask_id=mask_idx,
+            pad_id=padding_idx,
+            mask_prob=mask_probability,
+            vocab_low_high=(5, vocab_size),
+        )
 
         # Forward pass
-        all_token_likelihoods = transformer(masked_batch) # output: b,s,voc
-        
+        all_token_likelihoods = transformer(masked_batch)  # output: b,s,voc
+
         # Extract negative likelihoods of the masked tokens
-        ground_truth_likelihoods = all_token_likelihoods.gather(-1, batch.unsqueeze(-1)).squeeze()
+        ground_truth_likelihoods = all_token_likelihoods.gather(
+            -1, batch.unsqueeze(-1)
+        ).squeeze()
         masked_ground_truth_likelihoods = ground_truth_likelihoods[masked_batch_bool]
         log_likelihood = torch.neg(torch.log(masked_ground_truth_likelihoods))
 
@@ -181,11 +253,16 @@ for epoch in range(n_epochs):
         optimizer.step()
         checkpoint_losses.append((i, loss.item()))
 
-
-
     write_loss(checkpoint_losses, os.path.join(loss_dir, str(epoch)), loss_idx)
     loss_idx += 1
-    save_checkpoint(transformer, optimizer, epoch, filepaths, file_idx=0,
-                    checkpoint_dir=checkpoint_dir, max_checkpoints=max_checkpoints)
-    
+    save_checkpoint(
+        transformer,
+        optimizer,
+        epoch,
+        filepaths,
+        file_idx=0,
+        checkpoint_dir=checkpoint_epoch_dir,
+        max_checkpoints=None,
+    )
+
     checkpointed_files = 0
