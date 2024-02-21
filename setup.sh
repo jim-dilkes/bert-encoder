065a2f3259fb09ac253d1d3fbf5c54b5d507ce4a
jim-dilkes <jsdilkes@googlemail.com>
2024-02-21 01:10:12 +0000

    Remove unnecessary setup.sh file

--- setup.sh
+++
@@ -1,8 +0,0 @@
-git clone https://github.com/jim-dilkes/bert-encoder
-cd bert-encoder
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-apt update
-apt install unzip
-
-export HF_DATASETS_CACHE="/workspace/huggingface/datasets"
