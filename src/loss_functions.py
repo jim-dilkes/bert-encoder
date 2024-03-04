e3fca84f5bf7fa3f37a64e52274d54d3b3a3f514
jim-dilkes <jsdilkes@googlemail.com>
2024-03-04 20:54:08 +0000

    Refactor loss functions to improve readability and clarity

--- src/loss_functions.py
+++ src/loss_functions.py
@@ -1,20 +1,25 @@
 import torch
 
 
-def cross_entropy(all_token_likelihoods, masked_batch, masked_batch_bool):
-    masked_all_token_likelihoods = all_token_likelihoods[masked_batch_bool]
-    masked_actual_token_indices = masked_batch[masked_batch_bool]
+def cross_entropy(all_token_likelihoods, ground_truth_tokens, masked_batch_bool):
+    # Get the probability distributions of the masked token prediction
+    masked_token_distributions = all_token_likelihoods[masked_batch_bool]
+    # Get the actual token indices of the masked tokens
+    masked_actual_token_indices = ground_truth_tokens[masked_batch_bool]
+    # X-entropy loss using actual token indices and predicted token distributions
     loss = torch.nn.functional.cross_entropy(
-        masked_all_token_likelihoods,
+        masked_token_distributions,
         masked_actual_token_indices,
         reduction="mean",
     )
     return loss
 
 
-def negative_log_likelihood(all_token_likelihoods, masked_batch, masked_batch_bool):
+def negative_log_likelihood(
+    all_token_likelihoods, ground_truth_tokens, masked_batch_bool
+):
     actual_token_likelihoods = all_token_likelihoods.gather(
-        -1, masked_batch.unsqueeze(-1)
+        -1, ground_truth_tokens.unsqueeze(-1)
     ).squeeze()
     masked_actual_token_likelihoods = actual_token_likelihoods[masked_batch_bool]
     log_likelihood = torch.neg(torch.log(masked_actual_token_likelihoods))
